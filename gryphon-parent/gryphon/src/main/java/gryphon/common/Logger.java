3b5c42e917cdc83f5ec267ca9b58db6926007ae9
evg-tyurin <evg.tyurin@gmail.com>
2017-09-29 11:46:24 +0300

    5.3.3 ----- * Logger output can be redirected to java.util.logging.

--- gryphon-parent/gryphon/src/main/java/gryphon/common/Logger.java
+++ gryphon-parent/gryphon/src/main/java/gryphon/common/Logger.java
@@ -1,52 +1,95 @@
 package gryphon.common;
 
+import java.util.logging.Level;
+
 /**
  * <p>
  * Логгер
  * </p>
+ * 
  * @author Evgueni Tiourine
  */
 
-public class Logger {
-  public static final int DEBUG = 4;
-  public static final int INFO = 3;
-  public static final int WARN = 2;
-  public static final int ERR = 1;
-  public static final int NONE = 0;
+public class Logger
+{
+	public static final int DEBUG = 4;
+	public static final int INFO = 3;
+	public static final int WARN = 2;
+	public static final int ERR = 1;
+	public static final int NONE = 0;
 
-  private static int level = DEBUG;
+	private static int level = DEBUG;
 
-  public Logger() {
-  }
-  public static void init(int level) {
-    Logger.level = level;
-  }
-  public static void log(String s) {
-    debug(s);
-  }
-  public static void debug(String s) {
-    if (level >= DEBUG) {
-      System.out.println(s);
+    /** Логгер для перенаправления, если есть */
+    private static java.util.logging.Logger delegate;
+
+    public Logger() {
+	}
+
+	public static void init(int level)
+	{
+		Logger.level = level;
+	}
+
+	public static void log(String s)
+	{
+		debug(s);
+	}
+
+	public static void debug(String s)
+	{
+		if (level >= DEBUG) {
+			if (delegate==null)
+				System.out.println(s);
+			else
+				delegate.info(s);
+		}
+	}
+
+	public static void info(String s)
+	{
+		if (level >= INFO) {
+			if (delegate==null)
+				System.out.println(s);
+			else
+				delegate.info(s);
+		}
+	}
+
+	public static void warn(String s)
+	{
+		if (level >= WARN) {
+			if (delegate==null)
+				System.out.println(s);
+			else
+				delegate.warning(s);
+		}
+	}
+
+	public static void err(String s)
+	{
+		if (level >= ERR) {
+			if (delegate==null)
+				System.out.println(s);
+			else
+				delegate.severe(s);
+		}
+	}
+
+	public static void logThrowable(Throwable th)
+	{
+		if (delegate==null){
+			err(th.getClass().getName() + ": " + th.getMessage());
+			th.printStackTrace();
+		}
+		else{
+			delegate.log(Level.SEVERE, "", th);
+		}
+	}
+
+	/** Перенаправить вывод в java.util.logging.Logger для более гибкого управления */
+    public static void redirect(String loggerName){
+    	delegate = java.util.logging.Logger.getLogger(loggerName);
     }
-  }
-  public static void info(String s) {
-    if (level >= INFO) {
-      System.out.println(s);
-    }
-  }
-  public static void warn(String s) {
-    if (level >= WARN) {
-      System.out.println(s);
-    }
-  }
-  public static void err(String s) {
-    if (level >= ERR) {
-      System.out.println(s);
-    }
-  }
-  public static void logThrowable(Throwable th) {
-    err(th.getClass().getName()+": "+th.getMessage());
-    th.printStackTrace();
-  }
 
 }
